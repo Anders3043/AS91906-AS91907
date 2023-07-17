7f15d99671e92f537358457e5f0d94631237435e
Anders <st21184@ormiston.school.nz>
2023-07-18 10:02:06 +1200

    added colours

--- code.py
+++ code.py
@@ -1,36 +1,27 @@
 from tkinter import *
 
 
+# Function to run everything
 def main():
     global main_window
     main_window = Tk()
     main_window.geometry("1500x750")
+    main_window.config(bg="#094074")
     setup()
-    size()
     main_window.resizable(False, False)
     main_window.mainloop()
 
 
-def size():
-    width = 1500
-    height = 750
-
-    screen_width = main_window.winfo_screenwidth()  # Width of the screen
-    screen_height = main_window.winfo_screenheight() # Height of the screen
-
-    # Calculate Starting X and Y coordinates for Window
-    x = (screen_width/2) - (width/2)
-    y = (screen_height/2) - (height/2)
-
-    main_window.geometry('%dx%d+%d+%d' % (width, height, x, y))
-
-
+# Function for quit button
 def leave():
     main_window.destroy()
 
 
+# Setups for the buttons
 def setup():
-    Button(main_window, text="Quit", command=leave).grid(column=1, row=0)
+    Button(main_window, text="QUIT", command=leave, height=3, width=30, bg="#edae49").place(x=1138, y=550)
+    Button(main_window, text="ENTER NEW MATCH DETAILS", height=5, width=50, bg="#edae49").place(x=200, y=400)
+    Button(main_window, text="VISIT OLD MATCH DETAILS", height=5, width=50, bg="#edae49").place(x=1000, y=400)
 
 
 main()
