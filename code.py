065597ea9896b5c2a148c1d76f37e0a94c54737c
Anders <st21184@ormiston.school.nz>
2023-07-24 11:08:40 +1200

    Added Labels and resized entry boxes

--- code.py
+++ code.py
@@ -16,12 +16,6 @@ def main():
 def leave():
     main_window.destroy()
 
-
-# Function for quit button for first enter screen
-def leave1():
-    enter1.destroy()
-
-
 # Function for entering new details
 def enter():
     global enter1
@@ -29,15 +23,20 @@ def enter():
     enter1 = Tk()
     enter1.geometry("1500x750")
     enter1.config(bg="#094074")
-    Button(enter1, text="QUIT", command=leave1, height=3, width=30, bg="#edae49").place(x=1138, y=550)
+    # Entries
     team_a = Entry()
-    team_a.place(x=300, y=300)
+    team_a.place(x=300, y=200, width=300, height=80)
     team_b = Entry()
-    team_b.place(x=900, y=300)
+    team_b.place(x=930, y=200, width=300, height=80)
     date = Entry()
-    date.place(x=300, y=500)
+    date.place(x=300, y=450, width=300, height=80)
     location = Entry()
-    location.place(x=900, y=500)
+    location.place(x=930, y=450, width=300, height=80)
+    # Labels
+    Label(enter1, font="bold", text="Please enter name of team 1").place(x=300, y=150)
+    Label(enter1, font="bold", text="Please enter name of team 2").place(x=930, y=150)
+    Label(enter1, font="bold", text="Please enter the date of the game").place(x=300, y=400)
+    Label(enter1, font="bold", text="Please enter the location of the game").place(x=930, y=400)
 
 
 # Setups for the buttons
