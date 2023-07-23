5860dfd0e02f38506f029d122510af44eefca0c7
Anders <st21184@ormiston.school.nz>
2023-07-24 11:28:01 +1200

    Added Colours and Submit button

--- code.py
+++ code.py
@@ -16,27 +16,38 @@ def main():
 def leave():
     main_window.destroy()
 
+
 # Function for entering new details
 def enter():
-    global enter1
+    global enter1, team_a, team_b, date, location
     main_window.destroy()
     enter1 = Tk()
     enter1.geometry("1500x750")
     enter1.config(bg="#094074")
     # Entries
-    team_a = Entry()
+    team_a = Entry(bg="#306383")
     team_a.place(x=300, y=200, width=300, height=80)
-    team_b = Entry()
+    team_b = Entry(bg="#306383")
     team_b.place(x=930, y=200, width=300, height=80)
-    date = Entry()
+    date = Entry(bg="#306383")
     date.place(x=300, y=450, width=300, height=80)
-    location = Entry()
+    location = Entry(bg="#306383")
     location.place(x=930, y=450, width=300, height=80)
     # Labels
-    Label(enter1, font="bold", text="Please enter name of team 1").place(x=300, y=150)
-    Label(enter1, font="bold", text="Please enter name of team 2").place(x=930, y=150)
-    Label(enter1, font="bold", text="Please enter the date of the game").place(x=300, y=400)
-    Label(enter1, font="bold", text="Please enter the location of the game").place(x=930, y=400)
+    Label(enter1, font="bold", text="Please enter name of team 1", bg="#094074", fg="white").place(x=300, y=150)
+    Label(enter1, font="bold", text="Please enter name of team 2", bg="#094074", fg="white").place(x=930, y=150)
+    Label(enter1, font="bold", text="Please enter the date of the game", bg="#094074", fg="white").place(x=300, y=400)
+    Label(enter1, font="bold", text="Please enter the location of the game", bg="#094074", fg="white").place(x=930, y=400)
+    # Submit button
+    Button(enter1, text="Submit", command=sub1, height=3, width=30, bg="#edae49").place(x=930, y=600)
+
+
+# Submit button for the first page
+def sub1():
+    enter1.destroy()
+    enter2 = Tk()
+    enter2.geometry("1500x750")
+    enter2.config(bg="#094074")
 
 
 # Setups for the buttons
