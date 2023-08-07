c9066123da8fd3564453f235d73aef60c9e71a77
Anders <st21184@ormiston.school.nz>
2023-08-08 10:04:35 +1200

    Added a list to organise the data and used that data to show user

--- code.py
+++ code.py
@@ -49,7 +49,10 @@ def enter():
 
 # Submit button for the first page
 def sub1():
-    global team_a_mem1, team_b_mem1, enter2, team_a_goal, team_b_goal, team_a_box, team_b_box, date_box, location_box
+    global team_a_mem1, team_b_mem1, enter2, team_a_goal, team_b_goal, team_a_box, team_b_box, date_box, location_box, \
+        team_a_mem2, team_a_mem3, team_a_mem4, team_a_mem5, team_a_mem6, team_a_mem7, team_a_mem8, team_a_mem9, \
+        team_a_mem10, team_a_mem11, team_b_mem2, team_b_mem3, team_b_mem4, team_b_mem5, team_b_mem6, team_b_mem7, \
+        team_b_mem8, team_b_mem9, team_b_mem10, team_b_mem11
     # Creating variable for checking
     team_a_check = len(team_a.get())
     team_b_check = len(team_b.get())
@@ -74,7 +77,7 @@ def sub1():
         enter2.resizable(False, False)
         # Making new labels based on the entry
         Label(enter2, font='bold', text=team_a_box, bg='#003d5b', fg='white').place(x=300, y=160)
-        Label(enter2, font='bold', text="Please enter team member on each side", bg='#094074', fg='white').place(x=600, y=240)
+        Label(enter2, font='bold', text="Please enter team member on each side", bg='#094074', fg='white').place(x=580, y=240)
         Label(enter2, font='bold', text="Please enter goal scored on each side", bg='#094074', fg='white').place(x=600, y=520)
         Label(enter2, font='bold', text=team_b_box, bg='#003d5b', fg='white').place(x=1200, y=160)
         Label(enter2, font='bold', text=date_box, bg='#003d5b', fg='white').place(x=750, y=160)
@@ -132,6 +135,7 @@ def sub1():
         team_a_goal.place(x=220, y=600, width=200, height=75)
         team_b_goal = Entry(bg="#306383")
         team_b_goal.place(x=1120, y=600, width=200, height=75)
+        # Submit button
         Button(enter2, text="Submit", command=team_check, height=3, width=30, bg="#edae49").place(x=650, y=615)
 
 
@@ -157,60 +161,94 @@ def team_check():
     elif a_goal == "False" or b_goal == "False":
         Label(enter2, font='bold', text='Please make sure you have entered the goals each team score in numbers', bg='#094074', fg='red').place(x=450, y=550)
     else:
+        # Put player's name in a list
+        team_a_player = [team_a_mem1.get(), team_a_mem2.get(), team_a_mem3.get(), team_a_mem4.get(), team_a_mem5.get(),
+                         team_a_mem6.get(), team_a_mem7.get(), team_a_mem8.get(), team_a_mem9.get(), team_a_mem10.get(),
+                         team_a_mem11.get()]
+        team_b_player = [team_b_mem1.get(), team_b_mem2.get(), team_b_mem3.get(), team_b_mem4.get(), team_b_mem5.get(),
+                         team_b_mem6.get(), team_b_mem7.get(), team_b_mem8.get(), team_b_mem9.get(), team_b_mem10.get(),
+                         team_b_mem11.get()]
         # Create new page
         enter2.destroy()
         enter3 = Tk()
         enter3.geometry("1500x750")
         enter3.config(bg="#094074")
         enter3.resizable(False, False)
+        # Labels for new page
         Label(enter3, font='bold', text=team_a_box, bg='#003d5b', fg='white').place(x=300, y=160)
         Label(enter3, font='bold', text=team_b_box, bg='#003d5b', fg='white').place(x=1200, y=160)
         Label(enter3, font='bold', text=date_box, bg='#003d5b', fg='white').place(x=750, y=160)
         Label(enter3, font='bold', text=location_box, bg='#003d5b', fg='white').place(x=750, y=200)
+        Label(enter3, font='bold', text="Please enter goal scored by each member on each side", bg='#094074', fg='white').place(x=530, y=240)
+        Label(enter3, font='bold', text="(Remove the player's name then add the goal they scored)", bg='#094074',
+              fg='white').place(x=515, y=280)
+        # Entry boxes for goal scored by each member
         team_a_mem1_goal = Entry(bg="#306383")
         team_a_mem1_goal.place(x=220, y=250, width=200, height=30)
+        team_a_mem1_goal.insert(0, team_a_player[0])
         team_a_mem2_goal = Entry(bg="#306383")
         team_a_mem2_goal.place(x=220, y=280, width=200, height=30)
+        team_a_mem2_goal.insert(0, team_a_player[1])
         team_a_mem3_goal = Entry(bg="#306383")
         team_a_mem3_goal.place(x=220, y=310, width=200, height=30)
+        team_a_mem3_goal.insert(0, team_a_player[2])
         team_a_mem4_goal = Entry(bg="#306383")
         team_a_mem4_goal.place(x=220, y=340, width=200, height=30)
+        team_a_mem4_goal.insert(0, team_a_player[3])
         team_a_mem5_goal = Entry(bg="#306383")
         team_a_mem5_goal.place(x=220, y=370, width=200, height=30)
+        team_a_mem5_goal.insert(0, team_a_player[4])
         team_a_mem6_goal = Entry(bg="#306383")
         team_a_mem6_goal.place(x=220, y=400, width=200, height=30)
+        team_a_mem6_goal.insert(0, team_a_player[5])
         team_a_mem7_goal = Entry(bg="#306383")
         team_a_mem7_goal.place(x=220, y=430, width=200, height=30)
+        team_a_mem7_goal.insert(0, team_a_player[6])
         team_a_mem8_goal = Entry(bg="#306383")
         team_a_mem8_goal.place(x=220, y=460, width=200, height=30)
+        team_a_mem8_goal.insert(0, team_a_player[7])
         team_a_mem9_goal = Entry(bg="#306383")
         team_a_mem9_goal.place(x=220, y=490, width=200, height=30)
+        team_a_mem9_goal.insert(0, team_a_player[8])
         team_a_mem10_goal = Entry(bg="#306383")
         team_a_mem10_goal.place(x=220, y=520, width=200, height=30)
+        team_a_mem10_goal.insert(0, team_a_player[9])
         team_a_mem11_goal = Entry(bg="#306383")
         team_a_mem11_goal.place(x=220, y=550, width=200, height=30)
+        team_a_mem11_goal.insert(0, team_a_player[10])
         team_b_mem1_goal = Entry(bg="#306383")
         team_b_mem1_goal.place(x=1120, y=250, width=200, height=30)
+        team_b_mem1_goal.insert(0, team_b_player[0])
         team_b_mem2_goal = Entry(bg="#306383")
         team_b_mem2_goal.place(x=1120, y=280, width=200, height=30)
+        team_b_mem2_goal.insert(0, team_b_player[1])
         team_b_mem3_goal = Entry(bg="#306383")
         team_b_mem3_goal.place(x=1120, y=310, width=200, height=30)
+        team_b_mem3_goal.insert(0, team_b_player[2])
         team_b_mem4_goal = Entry(bg="#306383")
         team_b_mem4_goal.place(x=1120, y=340, width=200, height=30)
+        team_b_mem4_goal.insert(0, team_b_player[3])
         team_b_mem5_goal = Entry(bg="#306383")
         team_b_mem5_goal.place(x=1120, y=370, width=200, height=30)
+        team_b_mem5_goal.insert(0, team_b_player[4])
         team_b_mem6_goal = Entry(bg="#306383")
         team_b_mem6_goal.place(x=1120, y=400, width=200, height=30)
+        team_b_mem6_goal.insert(0, team_b_player[5])
         team_b_mem7_goal = Entry(bg="#306383")
         team_b_mem7_goal.place(x=1120, y=430, width=200, height=30)
+        team_b_mem7_goal.insert(0, team_b_player[6])
         team_b_mem8_goal = Entry(bg="#306383")
         team_b_mem8_goal.place(x=1120, y=460, width=200, height=30)
+        team_b_mem8_goal.insert(0, team_b_player[7])
         team_b_mem9_goal = Entry(bg="#306383")
         team_b_mem9_goal.place(x=1120, y=490, width=200, height=30)
+        team_b_mem9_goal.insert(0, team_b_player[8])
         team_b_mem10_goal = Entry(bg="#306383")
         team_b_mem10_goal.place(x=1120, y=520, width=200, height=30)
+        team_b_mem10_goal.insert(0, team_b_player[9])
         team_b_mem11_goal = Entry(bg="#306383")
         team_b_mem11_goal.place(x=1120, y=550, width=200, height=30)
+        team_b_mem11_goal.insert(0, team_b_player[10])
 
 
 main()
